da3439f6dae8e8592e8a9e0a7809d647e04498a2
Jocomol <joelmeier08@gmail.com>
2020-07-02 12:30:20 +0200

    added firefox

--- start.sh
+++ start.sh
@@ -3,3 +3,4 @@ docker kill my-running-app
 docker rm my-running-app
 docker build -t my-apache2 .
 docker run -dit --name my-running-app -p 8080:80 my-apache2
+firefox http://localhost:8080/main.html
