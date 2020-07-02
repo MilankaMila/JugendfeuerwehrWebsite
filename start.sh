2573c861176a43bf97a22d0bb71bbf01bd95e0ea
Jocomol <joelmeier08@gmail.com>
2020-07-02 12:37:34 +0200

    and back again

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo docker kill my-running-app
-sudo docker rm my-running-app
-sudo docker build -t my-apache2 .
-sudo docker run -dit --name my-running-app -p 8080:80 my-apache2
+docker kill my-running-app
+docker rm my-running-app
+docker build -t my-apache2 .
+docker run -dit --name my-running-app -p 8080:80 my-apache2
 firefox http://localhost:8080/main.html
